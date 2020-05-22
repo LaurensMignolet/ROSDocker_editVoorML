095fa5d499575e87ccca823851ec6a1f2b5e27df
Steve <steve.vanreeth@student.pxl.be>
2020-05-22 16:57:43 +0200

    added command to run catkin_make at launch of container

--- 01b_dri_ubuntu18.04/scripts/init_commands.sh
+++ 01b_dri_ubuntu18.04/scripts/init_commands.sh
@@ -2,4 +2,13 @@
 
 mkdir /tmp/myxdg
 sudo chown user:user /tmp/myxdg
+
+echo "setup melodic env"
+cd ../..
+. /opt/ros/melodic/setup.sh
+echo "melodic ended & setup catkin_ws env"
+cd home/user/Projects/catkin_ws
+catkin_make
+. devel/setup.sh
+echo "End setup"
 bash
